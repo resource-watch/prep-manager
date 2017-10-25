585b0561b3775548e17a0a7478c64349fbf3ecc1
bitTal <albafjez@gmail.com>
2017-10-25 18:10:27 +0200

    My prep header banner

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,60 +3,17 @@ import React from 'react';
 // Components
 import { Link } from 'routes';
 import MainNav from 'components/ui/MainNav';
-import Breadcrumbs from 'components/ui/Breadcrumbs';
-import Banner from 'components/app/common/Banner';
-
-// Constants
-import metadata from 'constants/metadata.json';
 
 export default class Header extends React.Component {
-  getData(key, value) {
-    let data = null;
-    // First search for exactly match
-    for (let i = metadata.length - 1; i >= 0; i--) {
-      if (value === metadata[i][key]) {
-        data = metadata[i];
-        break;
-      }
-    }
-    // If no data, search for close result
-    if (!data) {
-      for (let i = metadata.length - 1; i >= 0; i--) {
-        if (value.indexOf(metadata[i][key]) > -1) {
-          data = metadata[i];
-          break;
-        }
-      }
-    }
-
-    return data;
-  }
-
-  getCurrentData() {
-    // const pathname = this.props.location.pathname;
-    const pathname = 'myprep';
-    const currentData = this.getData('pathname', pathname);
-    return currentData;
-  }
-
   render() {
-    const currentData = this.getCurrentData();
-    const isHomepage = (currentData.name === 'home');
-    let pathname = '';
-    // const pathname = window.location.pathname;
-    // const summaryCardsPages = ['/contact', '/insights', '/dashboards'];
-
-    if (typeof document !== 'undefined') document.title = currentData.title;
-    if (typeof window !== 'undefined') pathname = window.location.pathname;
-
     return (
       <header className="l-header-app">
-        <div className={`l-header-nav ${currentData.name === 'home' ? '-no-bg' : ''}`}>
+        <div className="l-header-nav">
           <div className="row align-middle">
             <div className="column small-10 medium-4">
               <Link>
                 <a href="/" className="logo">
-                  <img src="static/images/logo-cms.png" alt="Partnership for Resilience and Preparedness" />
+                  <img src="/static/images/logo-cms.png" alt="Partnership for Resilience and Preparedness" />
                 </a>
               </Link>
             </div>
--- pages/admin/MyPREP.js
+++ pages/admin/MyPREP.js
@@ -10,6 +10,8 @@ import Page from 'components/admin/layout/Page';
 import Layout from 'components/layout/Layout';
 import Tabs from 'components/ui/Tabs';
 import Title from 'components/ui/Title';
+import Breadcrumbs from 'components/ui/Breadcrumbs';
+import Banner from 'components/app/common/Banner';
 
 // My RW
 import ProfilesTab from 'components/app/myprep/profiles/ProfilesTab';
@@ -19,6 +21,8 @@ import WidgetsTab from 'components/app/myprep/widgets/WidgetsTab';
 import AreasTab from 'components/app/myprep/areas/AreasTab';
 
 // Contants
+import metadata from 'constants/metadata.json';
+
 const MYPREP_TABS = [{
   label: 'Profile',
   value: 'profile',
@@ -68,11 +72,34 @@ class MyPREP extends Page {
     });
   }
 
+  getData(key, value) {
+    let data = null;
+    // First search for exactly match
+    for (let i = metadata.length - 1; i >= 0; i--) {
+      if (value === metadata[i][key]) {
+        data = metadata[i];
+        break;
+      }
+    }
+    // If no data, search for close result
+    if (!data) {
+      for (let i = metadata.length - 1; i >= 0; i--) {
+        if (value.indexOf(metadata[i][key]) > -1) {
+          data = metadata[i];
+          break;
+        }
+      }
+    }
+
+    return data;
+  }
+
   render() {
     const { url, user } = this.props;
     const { tab, subtab } = this.state;
     const userName = user && user.name ? ` ${user.name.split(' ')[0]}` : '';
     const title = `Hi${userName}!`;
+    const currentData = this.getData('pathname', 'myprep');
 
     return (
       <Layout
@@ -82,24 +109,24 @@ class MyPREP extends Page {
         user={user}
         pageHeader
       >
-        <div className="c-page-header">
-          <div className="l-container">
-            <div className="row">
-              <div className="column small-12">
-                <div className="page-header-content -with-tabs">
-                  <Title className="-primary -huge page-header-title" >
-                    {title}
-                  </Title>
-                  <Tabs
-                    options={MYPREP_TABS}
-                    defaultSelected={tab}
-                    selected={tab}
-                  />
-                </div>
-              </div>
-            </div>
-
-          </div>
+        <div className="c-page-header -myprep">
+          <Breadcrumbs pathname="myprep" />
+          <Banner
+            className="-myprep"
+            bgImage={currentData.bannerBg}
+            size={currentData.bannerSize}
+            landing={false}
+          >
+            <span></span>
+            <Title className="-primary -huge page-header-title -line" >
+              {currentData.title}
+            </Title>
+            <Tabs
+              options={MYPREP_TABS}
+              defaultSelected={tab}
+              selected={tab}
+            />
+          </Banner>
         </div>
         <div className="c-page-section">
           <div className="l-container">
